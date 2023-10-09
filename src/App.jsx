7c620204345b0724286fe0d618dc3a2aa53e2e15
liviu-o <li.viu@mail.com>
2023-10-09 11:05:08 +0100

    Add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1rem;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-  display: block;
 `;
 
 const StyledApp = styled.div`
@@ -32,11 +16,15 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button>Check in</Button>
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+    <>
+      {/* Note Global Styles does not accept any children */}
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 }
 
